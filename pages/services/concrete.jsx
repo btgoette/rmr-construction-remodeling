a249c99beac23ac15679195a534801f3599011c4
Nic <nic@brymels.com>
2025-02-10 18:02:58 -0800

    concrete and services page

--- pages/services/concrete.jsx
+++ pages/services/concrete.jsx
@@ -29,8 +29,8 @@ export default function ConcreteServices() {
                         <Col lg={{ span: 6, order: 1 }}>
                             <h2 data-aos="fade-up" className="text-start">Expert Concrete Services by RMR Construction & Remodeling</h2>
                             <div className="border-line-start"></div>
-                            <p>Enhance your home with high-quality, durable concrete solutions from RMR Construction & Remodeling. Whether you're upgrading your driveway, designing a custom patio, or installing walkways for improved accessibility, our expert team ensures precision and excellence in every project. We specialize in residential concrete installations, combining durability, aesthetics, and functionality to elevate your outdoor spaces.</p>
-                            <p>With top-tier materials and cutting-edge techniques, we deliver long-lasting, visually appealing concrete work that withstands time and weather. From consultation to completion, we prioritize your vision, crafting concrete solutions that enhance your home’s value and curb appeal.</p>
+                            <p>Enhance your home with high&#45;quality, durable concrete solutions from RMR Construction & Remodeling. Whether you&apos;re upgrading your driveway, designing a custom patio, or installing walkways for improved accessibility, our expert team ensures precision and excellence in every project. We specialize in residential concrete installations, combining durability, aesthetics, and functionality to elevate your outdoor spaces.</p>
+                            <p>With top&#45;tier materials and cutting&#45;edge techniques, we deliver long&#45;lasting, visually appealing concrete work that withstands time and weather. From consultation to completion, we prioritize your vision, crafting concrete solutions that enhance your home&apos;s value and curb appeal.</p>
                         </Col>
                     </Row>
                 </Container>
@@ -43,23 +43,23 @@ export default function ConcreteServices() {
                     <div className="border-line-center"></div>
                     <Row className="align-items-center py-2">
                         <Col lg={5}><h3 className="text-start text-white text-lg-end" data-aos="fade-right">Concrete Driveways</h3></Col>
-                        <Col lg={7}><p className="border-left" data-aos="fade-left">Upgrade your curb appeal with a custom concrete driveway that’s built to last. Designed for strength, durability, and low maintenance, our driveways withstand heavy use and harsh weather while offering a sleek, modern look.</p></Col>
+                        <Col lg={7}><p className="border-left" data-aos="fade-left">Upgrade your curb appeal with a custom concrete driveway that&apos;s built to last. Designed for strength, durability, and low maintenance, our driveways withstand heavy use and harsh weather while offering a sleek, modern look.</p></Col>
                     </Row>
                     <Row className="align-items-center py-2">
                         <Col lg={5}><h3 className="text-start text-white text-lg-end" data-aos="fade-right">Concrete Patios</h3></Col>
-                        <Col lg={7}><p className="border-left" data-aos="fade-left">Create an inviting outdoor space with a custom-designed patio. Whether you're hosting guests or enjoying a quiet evening, our patios provide beauty, comfort, and functionality that complement your home’s style.</p></Col>
+                        <Col lg={7}><p className="border-left" data-aos="fade-left">Create an inviting outdoor space with a custom&#45;designed patio. Whether you&apos;re hosting guests or enjoying a quiet evening, our patios provide beauty, comfort, and functionality that complement your home&apos;s style.</p></Col>
                     </Row>
                     <Row className="align-items-center py-2">
                         <Col lg={5}>
                             <h3 className="text-start text-white text-lg-end" data-aos="fade-right">Sidewalks & Pathways</h3>
                         </Col>
                         <Col lg={7}
-                        ><p className="border-left" data-aos="fade-left">Improve accessibility and boost your landscape’s charm with expertly crafted concrete sidewalks and pathways. Our seamless installations ensure both safety and aesthetic appeal.</p>
+                        ><p className="border-left" data-aos="fade-left">Improve accessibility and boost your landscape&apos;s charm with expertly crafted concrete sidewalks and pathways. Our seamless installations ensure both safety and aesthetic appeal.</p>
                         </Col>
                     </Row>
                     <Row className="align-items-center py-2">
                         <Col lg={5}><h3 className="text-start text-white text-lg-end" data-aos="fade-right">Concrete Pool Decks</h3></Col>
-                        <Col lg={7}><p className="border-left" data-aos="fade-left">Enhance your pool area with a slip-resistant, stylish pool deck. Built for durability and comfort, our concrete pool decks create the perfect space for relaxation and entertainment.</p>
+                        <Col lg={7}><p className="border-left" data-aos="fade-left">Enhance your pool area with a slip&#45;resistant, stylish pool deck. Built for durability and comfort, our concrete pool decks create the perfect space for relaxation and entertainment.</p>
                         </Col>
                     </Row>
                     <Row className="align-items-center py-2">
@@ -69,7 +69,7 @@ export default function ConcreteServices() {
                     </Row>
                     <Row className="align-items-center py-2">
                         <Col lg={5}><h3 className="text-start text-white text-lg-end" data-aos="fade-right">Concrete Replacement</h3></Col>
-                        <Col lg={7}><p className="border-left" data-aos="fade-left">Is your concrete cracked or deteriorating? Our concrete replacement services restore your surfaces by removing damaged concrete and installing fresh, high-quality replacements built to last.</p></Col>
+                        <Col lg={7}><p className="border-left" data-aos="fade-left">Is your concrete cracked or deteriorating? Our concrete replacement services restore your surfaces by removing damaged concrete and installing fresh, high&#45;quality replacements built to last.</p></Col>
                     </Row>
                     <Row className="align-items-center py-2">
                         <Col lg={5}><h3 className="text-start text-white text-lg-end" data-aos="fade-right">Concrete Repair</h3></Col>
